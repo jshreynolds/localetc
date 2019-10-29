3c484b01332f6aafe023596fafbae4a0f7d69d86
Josh Reynolds <jreynolds@electrichand.com>
2019-10-29 10:34:09 -0400

    Updating install script to be a standalone download

--- install/install.sh
+++ install/install.sh
@@ -2,14 +2,40 @@
 
 if [ $# -ne 1 ] 
 then
-   echo "please provide a name for your machine"
+   echo "please provide a name for your machine as an argument to this script"
+   exit 1
 fi 
+
+# Ask for the administrator password upfront
+echo "Enter your admin password at the prompt..."
+echo
 
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 set name = $1
 
-./bootstrap.sh
+# Generate ssh key
+ssh-keygen
+
+echo
+read -p "Upload the public ssh key just generated [~/.ssh/id_rsa.pub] to github and hit Enter..."
+echo
+
+# Install xcode tools
+xcode-select --install
+
+echo
+read -p "Install xcode command-line tools. Hit Enter when it's done..."
+echo
+
+#get the repo and do all the things!
+
+git clone git@github.com:jshreynolds/localetc.git ~/etc
+
+pushd ~/etc
 ./brew.sh
 ./cc.sh
 ./ohmyzsh.sh
@@ -18,3 +44,4 @@ set name = $1
 ./sdkman.sh
 ./vim.sh
 ./macos.sh $name
+popd
