1ef89ac4f56fdc461cf1bb08c5177a002048ba61
Josh Reynolds <jreynolds@electrichand.com>
2022-01-03 16:56:41 -0600

    adding in more minor adaptations

--- install/99-ohmyzsh.sh
+++ install/99-ohmyzsh.sh
@@ -4,12 +4,14 @@ echo
 echo "Installing oh my zsh!.."
 echo
 
-# Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+mkdir -p ~/.oh-my-zsh/themes
 ln -s ~/etc/dotfiles/jshlyd.zsh-theme ~/.oh-my-zsh/themes
 mv ~/.zshrc ~/.zshrc.bak
 ln -s ~/etc/dotfiles/zshrc ~/.zshrc
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 
 echo
 echo "Done with oh my zsh!"
