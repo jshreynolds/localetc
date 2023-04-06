cb8e3c91da57c6247f998f172a00d3080b76e6f9
Joshua Reynolds <jreynolds@electrichand.com>
2023-04-06 21:40:30 +0200

    minor tweaks

--- install/10-asdf.sh
+++ install/10-asdf.sh
@@ -1,15 +1,10 @@
-#! /bin/zsh
+#! /bin/bash
 
 #Java has no numerical latest so this must be done manually
 asdf plugin-add java
 latest=$(asdf latest java "adoptopenjdk-[0-9]")
 asdf install java $latest
 asdf global java $latest
-
-#nodejs must have it's gpg key imported
-asdf plugin add nodejs
-bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-
 
 export R_EXTRA_CONFIGURE_OPTIONS='--enable-R-shlib --with-cairo'
 export PACKS="clojure elixir golang haskell helm kubectl nodejs poetry python ruby rust scala terraform"
